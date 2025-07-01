62b150605db3012b3ecacb032fac8e73d9eda649
Daeun Kim <150661115+dani1552@users.noreply.github.com>
2025-07-01 14:49:35 +0900

    feat: 웹소켓 송수신 메세지 타입 정의

---
+++ src/types/WebSocketType.ts
@@ -0,0 +1,29 @@
+export type InitUserMessage = number;
+
+export interface ReceiveMessage {
+  roomId: number;
+  senderId: number;
+  message: string;
+  sendAt: string;
+}
+
+export interface SendMessage {
+  roomId: number;
+  receiverUserId: number;
+  message: string;
+  sendAt: string;
+}
+
+export interface MarkAsRead {
+  roomId: number;
+}
+
+// 서버 → 클라이언트
+export type WebSocketIncomingMessage =
+  | { event: 'init_user'; data: InitUserMessage }
+  | { event: 'receive_message'; data: ReceiveMessage };
+
+// 클라이언트 → 서버
+export type WebSocketOutgoingMessage =
+  | { event: 'send_message'; data: SendMessage }
+  | { event: 'mark_as_read'; data: MarkAsRead };
