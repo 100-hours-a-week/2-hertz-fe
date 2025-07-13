ced81b34a8ed1ae8980ed93f14c4b0a7600fb815
Daeun Kim <150661115+dani1552@users.noreply.github.com>
2025-07-13 15:13:34 +0900

    SSE 이벤트 미감지 이슈 해결 및 핸들러 리팩토링 (#143)
    * refactor: NewMessageType 타입 분리
    
    * refactor: SSE 핸들러 분리 및 SSE 이벤트 미감지 이슈 해결
    
    - getSSEHandlers 내 window.location.pathname 사용으로 실시간 채팅방 감지
    - 최초 채팅방 ID 고정되는 문제 해결
    - SSE 핸들러 생성 시 pathname 클로저 참조 제거
    - 관련 로직 리팩토링 및 안정성 향상

---
+++ src/types/chat.ts
@@ -0,0 +1,8 @@
+export type NewMessageType = {
+  channelRoomId: number;
+  partnerId: number;
+  partnerNickname: string;
+  message: string;
+  messageSendAt: string;
+  partnerProfileImage: string;
+};
